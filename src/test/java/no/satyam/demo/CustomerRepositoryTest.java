13951562dc980b66cb429bd8d56f7ef3861728f8
jabrena <bren@juanantonio.info>
2021-08-11 09:11:20 +0000

    Fixing issue with test execution; adding github action

--- src/test/java/no/satyam/demo/CustomerRepositoryTest.java
+++ src/test/java/no/satyam/demo/CustomerRepositoryTest.java
@@ -1,17 +1,15 @@
 package no.satyam.demo;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @ContextConfiguration(initializers = DockerPostgreDataSourceInitializer.class)
 public class CustomerRepositoryTest {
+    
     @Autowired
     CustomerRepository repository;
 
